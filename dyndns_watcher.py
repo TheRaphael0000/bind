96e85219648da49fda3cfbece98e40ed99aed136
Raphaël Margueron <raphael.margueron@gmail.com>
2023-01-05 15:46:44 +0100

    use ipecho instead of upnpclient since it doesn't always work

--- dyndns_watcher.py
+++ dyndns_watcher.py
@@ -5,18 +5,17 @@ import os
 import pathlib
 import tempfile
 import urllib.request
-import upnpclient
 
 from jinja2 import Template
 
 last_ip_filename = pathlib.Path(tempfile.gettempdir()) / "last_ip"
 bind_zone_folder = "/etc/bind/zones/"
-upnp_descriptor_url = "http://192.168.0.254:60000/b54e41a6/gatedesc.xml"
+ip_fetcher = "https://ipecho.net/plain"
 no_ip_update_route = "https://dynupdate.no-ip.com/nic/update"
 
 config = json.load(open("config.json"))
 
-user_agent = "TheRaphael0000 dyndns_watcher/1.0 raphael.margueron@gmail.com"
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
 
 # get previous ip
 try:
@@ -24,8 +23,8 @@ try:
 except FileNotFoundError:
     previous_ip = None
 
-router = upnpclient.Device(upnp_descriptor_url)
-current_ip = router.WANIPConn1.GetExternalIPAddress()["NewExternalIPAddress"]
+current_ip = urllib.request.urlopen(ip_fetcher).read().decode("utf-8")
+print(current_ip)
 
 # nothing to do if the last check was
 if previous_ip == current_ip:
@@ -40,7 +39,7 @@ print("NO-IP IP CHANGE REQUEST")
 url = f"{no_ip_update_route}?hostname={','.join(config['noip_ddns'])}&myip={current_ip}"
 print(url)
 request = urllib.request.Request(url)
-user_pass = f"{config['noip_user']}:{config['noip_pass']}".encode("ascii")
+user_pass = f"{config['noip_user']}:{config['noip_pass']}".encode("utf-8")
 b64_user_pass = base64.b64encode(user_pass)
 request.add_header("Authorization", f"Basic {b64_user_pass}")
 request.add_header("User-Agent", user_agent)
